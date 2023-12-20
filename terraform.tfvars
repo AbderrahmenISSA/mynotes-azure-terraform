fbf65eba587418c15d906cb30c4fcb2ca27d2d7d
Ahed Bahri <ahedelbahri@gmail.com>
2023-12-20 08:21:42 +0100

    azurerm resource provisioning

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+subscription_id      = "2f70ea30-d0b7-40b8-8aa2-a0bbb69923c2"
+admin_password       = "Testing@1912"
+mysql_admin_password = "my$qladmin2023"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "admin_password" {
+  description = "Admin password for virtual machines and MySQL server"
+}
+
+variable "mysql_admin_password" {
+  description = "Admin password for MySQL server"
+}
