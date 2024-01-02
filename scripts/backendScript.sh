eda08907f6b144b9b4b6284845c57cf6a2d61699
Ahed Bahri <ahedelbahri@gmail.com>
2024-01-02 20:30:40 +0100

    adding service principal

--- scripts/backendScript.sh
+++ scripts/backendScript.sh
@@ -5,17 +5,24 @@ sudo apt install -y \
     ca-certificates \
     curl \
     software-properties-common
-
-# Install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
+
 # Install Azure CLI 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# Pull latest docker image 
-sudo docker pull 
+#ServicePlan ACR Connection 
+ACR_REGISTRY="*********.azurecr.io"
+ACR_SP_ID="*********************"
+ACR_SP_SECRET="*********************"
 
+# Log in to ACR using service principal credentials
+sudo docker login $ACR_REGISTRY -u $ACR_SP_ID -p $ACR_SP_SECRET
+
+# Pull the container image
+sudo docker pull $ACR_REGISTRY/notes-apis:latest
+sudo docker run -d -p 8080:8080 --name my_notes_api $ACR_REGISTRY/notes-apis:latest
